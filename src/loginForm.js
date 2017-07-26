b84f3a2c41b7eacdcc7cc5bf4c4d406a003dd2bd
kevingatera <kevingatera@gmail.com>
2017-07-26 16:43:50 -0400

    StatusBar color Changed + bug fix in autoCorect on Login.js

--- src/loginForm.js
+++ src/loginForm.js
@@ -1,34 +1,116 @@
 import React, { Component} from 'react';
-import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import {
+    StyleSheet,
+    View, TextInput, Picker,
+    TouchableOpacity, Text
+} from 'react-native';
 
 export default class LoginForm extends Component {
     render(){
         return (
             <View style={styles.container}>
+                {/* <Picker
+                    selectedValue={this.state.language}
+                    onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+                    <Picker.Item label="Java" value="java" />
+                    <Picker.Item label="JavaScript" value="js" />
+                </Picker> */}
                 <TextInput underlineColorAndroid='rgba(0,0,0,0)'
                     keyboardType="email-address" // determine the type of keyBoard to Username to use
                     autoCapitalize="none" // Mostly for iOS
-                    autoCorrect="none" // Mostly for iOS
-                    placeholder="Username or email"
+                    autoCorrect={false} // Mostly for iOS
+                    placeholder="Enter your username"
                     placeholderTextColor="rgba(28, 133, 184, 0.48)"
                     returnKeytype="next" // This changes the returnKey label in iOS
                     // The line below instructs the cursor to move to the next field
+                    ref={(input) => this.emailInput = input}
+                    onSubmitEditing = {() => this.emailInput.focus()}
+                    onChangeText = {(username) => this.setState({username})}
+                    value={this.state.username}
+                    style={styles.input}
+                />
+
+                <TextInput underlineColor='rgba(0,0,0,0)'
+                    keyboardType = "email-address"
+                    autoCapitalize = "none"
+                    autoCorrect = {false}
+                    placeholder = "Enter your email"
+                    placeholderTextColor = 'rgba(28, 133, 184, 0.48)'
+                    returnKeytype="next"
+                    ref={(input) => this.emailInput = input}
                     onSubmitEditing = {() => this.passwordInput.focus()}
-                    style={styles.input} />
+                    onChangeText = {(email) => this.setState({email})}
+                    value={this.state.email}
+                    style={styles.input}
+                />
+
                 <TextInput underlineColorAndroid='rgba(0,0,0,0)'
                     placeholderTextColor="rgba(28, 133, 184, 0.48)"
                     secureTextEntry // This hides the password input
                     returnKeytype="go" // This changes the returnKey label in iOS
-                    placeholder="password"
-                    // Function to Assign the input to a variable for storage purposes
+                    placeholder="Enter your password"
+                    onChangeText = {(password) => this.setState({password})}
+                    value={this.state.password}
+                    // Function to Assign the input to a variable for reference purposes
                     ref={(input) => this.passwordInput = input }
                     style={styles.input} />
-                <TouchableOpacity style={styles.loginButton}>
-                    <Text style={styles.loginButtonText}>LOGIN</Text>
+                <TouchableOpacity onPress={loginUser} style={styles.loginButton}>
+                    <Text style={styles.loginButtonText}> Signup </Text>
                 </TouchableOpacity>
             </View>
         );
     }
+
+    /* The followwing will define state in order to avoid the "null is not an object" error */
+
+    constructor(props){
+        super(props);
+        this.state = {
+            username: null,
+            email: null,
+            password: null,
+        }
+
+        loginUser = () => {
+            // alert(this.state.password); // This is a test line
+            fetch('https://kevingatera.com/api/users', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: this.state.username,
+                    email: this.state.email,
+                    password: this.state.password
+                })
+            })
+            .then(ApiUtils.checkStatus)
+            .then((response) => response.json())
+            .then((res) => {
+                if(res.success === true) {
+                    alert(res.message)
+                } else {
+                    alert(res.message)
+                }
+            }).catch(function(error) {
+                alert('There has been a problem with your fetch operation: ' + error.message);
+            })
+        };
+
+        var ApiUtils = {
+          checkStatus: function(response) {
+            if (response.status >= 200 && response.status < 300) {
+              return response;
+            } else {
+              let error = new Error(response.statusText);
+              error.response = response;
+              throw error;
+            }
+          }
+        };
+    }
+
 }
 
 const styles = StyleSheet.create({
